62d8540491a595801c8852812110ad7540756b42
pengode-handal <saifpatih@gmail.com>
2024-03-03 20:49:33 +0700

    testimoni

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+// const path = require("path");
+const utils = require("./utils");
+
+// app.use(express.static(path.resolve(__dirname, "..", "client")));
+
+// Testimoni untuk qr scanner dan qr generator
+// app.get("/qr", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "..", "client", "index.html"));
+// })
+
+// Mengecek keberadaan data sekaligus status expirednya
+app.get("/api/check", async (req, res) => {
+    res.json(await utils.checkItem(req.query.code))
+})
+
+// Index website
+app.get("/", (req, res) => {
+    res.json({
+        message: "Swslqmoqt datang"
+    });
+})
+
+// Menambahkan data kedalam database dengan parameter query site.com/api/add?nama=value&email=value
+app.get("/api/add", async (req, res) => {
+    try {
+        // if (window.frames.top.document.referrer.includes('eventpadz')){res.json(await createData(req.params.nama, req.params.email));}
+        res.json(await utils.createData(req.query.nama, req.query.email));
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong: "+error,
+        })
+    }
+})
+
+// Rest API yang digunakan ketika scanning qr membuat status isExp dari unique code menjadi true
+app.get('/api/done', async (req, res) => {
+    const code = req.query.code;
+    try {
+        res.json(await utils.updateData(code, true));
+    } catch (error) {
+        res.status(500).json({
+            message: "Error getting "+error
+        })
+    }
+})
+
+// Rest API yang membuat status isExp dari unique code menjadi false
+app.get('/api/undone', async (req, res) => {
+    const code = req.query.code;
+    try {
+        res.json(await utils.updateData(code, false))
+    } catch (error) {
+        res.status(500).json({
+            message: "Error getting: "+error
+        })
+    }
+})
+
+// Menghapus data tertentu berdasrakan unique code
+app.get('/api/delete', async (req, res) => {
+    const code = req.query.code;
+    try {
+        res.json(await utils.deleteItem(code));
+    } catch (error) {
+        res.status(500).json({
+            message: "Error getting: "+error
+        }); return;
+    }
+})
+
+// Filter berdasarkan kunci properti pengembalikan dalam bentuk array
+app.get('/api/filter', async (req, res) => {
+    try {
+        const emails = await utils.getSelectedItems(req.query.q)
+        res.json(emails.map(user => user[req.query.q]))
+    } catch (error) {
+        res.status(500).json({
+            message: "Error getting: "+error
+        }); return;
+    }
+})
+
+// Home landing page REST API yang berisi seluruh list database kakel
+app.get("/api", async (req, res) => {
+    try {
+        res.json(await utils.getAllItems())
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong: "+error
+        }); return;
+    }
+})
+
+// Menjalankan di port 1337
+app.listen(1337);
+
+module.exports = app;
---
+++ utils.js
@@ -0,0 +1,96 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+//fungsi random untuk unique code
+function rand(min, max) {
+    return Math.floor((Math.random() * (max-min+1)) + min);
+}
+
+//Membuat dan memasukkan data ke dalam database
+const createData = async (nama, email) => {
+    //deklarasi db
+    const response = await prisma.kakel.create({
+        data: {
+            name: nama,
+            email: email,
+            //mengisi code dengan angka random
+            code: rand(0,9999).toString()
+        }
+    })
+    return response
+}
+
+// Cari item berdasarkan kode tertentu
+async function checkItem(code) {
+    const item = await prisma.kakel.findUnique({
+        where: {
+            code: code,
+        },
+    });
+
+    // Jika item ditemukan dan isExp masih false, lakukan hal ini
+    if (item) {
+        if (item.isExp === false) {
+            return {
+                message: 'Msih Bisa Dipake',
+                data: item
+            }
+        } else {
+            return {
+                    message: 'Ga Bisa Dipake',
+                    data: item
+            }
+        }
+    } else {
+        return {
+            message: "Data Tidak Ditemukan"
+        }
+    }
+}
+
+//Update database dengan memasukkan unique code lalu mengubah value dari isExp menjadi true atau false
+const updateData = async (code, trufal=true) => {
+    await prisma.kakel.update({
+        where: {
+            code: code
+        },
+        data: {
+            isExp: trufal,
+        }
+    })
+    return {
+        message: "Successfully updated",
+        data: await checkItem(code)
+    }
+}
+
+const deleteItem = async (code) => {
+    if (!code) return {message: "Isi dulu"}
+    await prisma.kakel.delete({
+        where: {
+            code: code,
+        }
+    })
+    return {message: "Succesfully deleted"}
+}
+
+const getAllItems = async () => {
+    return await prisma.kakel.findMany()
+}
+
+const getSelectedItems = async (key) => {
+    return await prisma.kakel.findMany( {
+        select: {[key]: true}
+    })
+}
+
+
+module.exports = {
+    createData,
+    checkItem,
+    updateData,
+    deleteItem,
+    rand,
+    getAllItems,
+    getSelectedItems
+}
